248e392add73c51cd97d144df3ab665e671c2bf9
liviu.petri <liviu.petri@yahoo.com>
2013-11-27 18:20:08 +0200

    test suites, etc

--- Gemfile
+++ Gemfile
@@ -3,35 +3,38 @@ ruby '2.0.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
-
-# Use sqlite3 as the database for Active Record
+gem 'pg'
+# Use (sqlite3) postgres as the database for Active Record
 group :development, :test do
-	gem 'sqlite3'
-	gem 'rspec-rails'
+	gem 'rspec-rails', '2.13.1'
+	gem 'guard-rspec', '2.5.0'
+	gem 'spork-rails', '4.0.0'
+	gem 'guard-spork', '1.5.0'
+	gem 'childprocess', '>=0.3.6'
 end
 
 group :test do
-	gem 'selenium-webdriver'
-	gem 'capybara'
+	gem 'selenium-webdriver', '2.35.1'
+	gem 'capybara', '2.1.0'
 end
 
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.0'
+gem 'sass-rails', '~> 4.0.1'
 
 # Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
+gem 'uglifier', '>= 2.1.1'
 
 # Use CoffeeScript for .js.coffee assets and views
-gem 'coffee-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.0.1'
 
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
-gem 'jquery-rails'
+gem 'jquery-rails', '3.0.4'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+gem 'turbolinks', '1.1.1'
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
@@ -42,7 +45,6 @@ group :doc do
 end
 
 group :production do
-	gem 'pg'
 	gem 'rails_12factor'
 end
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+	let(:base_title) {"T3App"}
   
   describe "Home page" do
 
   	it "should have the title Home" do
-  		visit 'static_pages/home'
-  		expect(page).to have_title('T3App | Home')
+  		visit '/static_pages/home'
+  		expect(page).to have_title("#{base_title} | Home")
   	end
   	it "should have the content 'T3 App'" do
   		visit '/static_pages/home'
@@ -16,8 +17,8 @@ describe "StaticPages" do
 
   describe "Help page" do
   	it "should have the title 'Help'" do
-  		visit 'static_pages/help'
-  		expect(page).to have_title('T3App | Help')
+  		visit '/static_pages/help'
+  		expect(page).to have_title("#{base_title} | Help")
   	end
 
   	it "shold have the content 'Help" do
@@ -28,13 +29,25 @@ describe "StaticPages" do
 
   describe "About page" do
   	it "should have the title 'About'" do
-  		visit 'static_pages/about'
-  		expect(page).to have_title('T3App | About')
+  		visit '/static_pages/about'
+  		expect(page).to have_title("#{base_title} | About")
   	end
-  	it "should have the title About us" do
+  	it "should have the content About us" do
   		visit '/static_pages/about'
   		expect(page).to have_content "About us"
   	end
   end
+
+  describe "Contact page" do
+  	it "Should have title 'Contact'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_title "#{base_title} | Contact"
+  	end
+  	it "Should have content contact" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content "contact"
+  	end
+  end
+
 
 end
