f8642cdabc8260d0f32b5b11dc14a8888d4de9b0
liviu.petri <liviu.petri@yahoo.com>
2013-11-29 08:54:30 +0200

    done w/ layout'

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,53 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-	let(:base_title) {"T3App"}
-  
-  describe "Home page" do
+describe "Static pages" do
 
-  	it "should have the title Home" do
-  		visit '/static_pages/home'
-  		expect(page).to have_title("#{base_title} | Home")
-  	end
-  	it "should have the content 'T3App'" do
-  		visit '/static_pages/home'
-  		expect(page).to have_content('T3App')
-  	end
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_content('T3App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-  	it "should have the title 'Help'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_title("#{base_title} | Help")
-  	end
+    before { visit help_path }
 
-  	it "shold have the content 'T3App'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content "T3App"
-  	end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
-  	it "should have the title 'About'" do
-  		visit '/static_pages/about'
-  		expect(page).to have_title("#{base_title} | About")
-  	end
-  	it "should have the content About us" do
-  		visit '/static_pages/about'
-  		expect(page).to have_content "About us"
-  	end
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
-  	it "Should have title 'Contact'" do
-  		visit '/static_pages/contact'
-  		expect(page).to have_title "#{base_title} | Contact"
-  	end
-  	it "Should have content contact" do
-  		visit '/static_pages/contact'
-  		expect(page).to have_content "contact"
-  	end
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
-
-
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# construct the full title based on page title 
+def full_title(page_title)
+	base_title = "T3App"
+	if page_title.empty?
+		base_title
+	else
+		"#{base_title} | #{page_title}"
+	end
+end
